d84ea592dfdf31e3e9a3906013ed974efb5acfbd
Daniel Romero <daanmaro99june@gmail.com>
2022-07-20 15:20:54 -0500

    Finish navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// const hamburger = document.querySelector(".nav-icon-menu")
+
+// hamburger.addEventListener("click", () => {
+//   hamburger.classList.toggle("active");
+// })
+
 /* example function name */
 const openAndCloseAccordion = () => {
   alert ('do something')
